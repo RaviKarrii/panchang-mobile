4119a99d7ef1fc2393e0848f54541cefebd2e8c1
Ravi Shanker Reddy Karri <me@ravikarri.in>
2020-02-24 20:16:50 +0530

    Adding Calendar

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Calendar } from 'react-native-calendars';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class Example extends React.Component {
+  render() 
+  {
+    return (
+      <View style={{ paddingTop: 50, flex: 1 }}>
+        <Calendar
+          // Initially visible month. Default = Date()
+          current={'2020-02-22'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={day => {
+            console.log('selected day', day);
+          }}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'MMM yyyy'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={month => {
+            console.log('month changed', month);
+          }}
+          // Hide month navigation arrows. Default = false
+          hideArrows={false}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={false}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={1}
+        />
+      </View>
+    );
+  }
+}
