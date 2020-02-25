10f981c9fc345528b267079bf65978fc163a484b
Ravi Shanker Reddy Karri <me@ravikarri.in>
2020-02-25 22:28:27 +0530

    FEw Updates

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Calendar } from 'react-native-calendars';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, ScrollView, Text, View } from 'react-native';
 
 
 export default class Example extends React.Component {
   constructor(props){
     super(props)
     //set value in state for initial date
-    this.state = {date:""}
+    this.state = {date:"",
+                  jsondata:{}}
   }
   saveState(data){
     this.setState({date: data.dateString})
   };
+  getMoviesFromApiAsync() {
+    return fetch('https://daily-panchang.herokuapp.com/panchang-api/v1.0/?date=2020-02-25&location=Kakinada')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.movies;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
   
   render() 
   {
@@ -24,7 +35,9 @@ export default class Example extends React.Component {
           onDayPress={day => {
             console.log(day);
             this.saveState(day);
-            console.log(this.state.date)
+            this.setState({jsondata :this.getMoviesFromApiAsync()})
+            console.log(this.state.jsondata);
+            //console.log(this.state.date)
           }}
           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
           monthFormat={'MMM yyyy'}
@@ -42,6 +55,14 @@ export default class Example extends React.Component {
           // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
           firstDay={1}
         />
+        <ScrollView>       
+          <Text>
+            {this.state.date}
+          </Text> 
+          <Text>{this.state.jsondata.Durmuhurtam}</Text>
+          <Text>{this.state.jsondata.Karna}</Text>
+          <Text>{this.state.jsondata.Karna}</Text>     
+        </ScrollView>
       </View>
     );
   }
