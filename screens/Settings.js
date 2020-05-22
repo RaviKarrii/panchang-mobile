92bfe5c01ebfa9a542a285da31b61ca5756790ac
Ravi Shanker Reddy Karri <me@ravikarri.in>
2020-05-23 00:03:47 +0530

    Update Settings.js

--- screens/Settings.js
+++ screens/Settings.js
@@ -8,7 +8,8 @@ export class Settings extends React.Component {
           <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text> Settings </Text>
           </View>
-        );
+        ); 
+        
       }
     
 }
