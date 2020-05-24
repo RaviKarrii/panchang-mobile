38c36a0cee9d43abaad7c5029b2a396c88d54e7a
Ravi Shanker Reddy Karri <me@ravikarri.in>
2020-05-24 14:55:57 +0530

    Adjusting Formatting

--- screens/locationdata.js
+++ screens/locationdata.js
@@ -1,10 +1,10 @@
 module.exports = {
-    data : [
-        {id:'1', name:'Paris'},
-        {id:'2', name: 'Pattanduru'},
-        {id:'3', name: 'Para'},
-        {id:'4', name:'London'},
-        {id:'5', name:'New York'},
-        {id:'6', name:'Berlin'}
+    data: [
+        { id: '1', name: 'Paris' },
+        { id: '2', name: 'Pattanduru' },
+        { id: '3', name: 'Para' },
+        { id: '4', name: 'London' },
+        { id: '5', name: 'New York' },
+        { id: '6', name: 'Berlin' }
     ],
- };
+};
--- screens/marriage.js
+++ screens/marriage.js
@@ -14,7 +14,7 @@ import { Button } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import GLOBAL from './locationdata.js';
-import MapView, { PROVIDER_GOOGLE , Marker} from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 
 
@@ -29,21 +29,28 @@ export class marriage extends React.Component {
     //true: You can expand multiple at a time
     //false: One can be expand at a time and other will be closed automatically
     multipleSelect: true,
-    Bcoord:{
+    Bcoord: {
       latitude: 53.41058,
       longitude: -2.97794,
       latitudeDelta: 0.1,
       longitudeDelta: 0,
-  }
-,
-    Bdate:"2016-05-15",
-    Btime:"00:00",
-    Gdate:"2016-05-15",
-    Gtime:"00:00",
+    }
+    ,
+    Gcoord: {
+      latitude: 53.41058,
+      longitude: -2.97794,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0,
+    }
+    ,
+    Bdate: "2016-05-15",
+    Btime: "00:00",
+    Gdate: "2016-05-15",
+    Gtime: "00:00",
   };
 
 
-  
+
 
   toggleExpandedBoy = () => {
     //Toggling the state of single Collapsible
@@ -62,23 +69,28 @@ export class marriage extends React.Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if(prevState.Btime !== this.state.Btime) {
-      this.setState({collapsedBoy: true})
+    if (prevState.Btime !== this.state.Btime) {
+      this.setState({ collapsedBoy: true })
     }
-    if(prevState.Gtime !== this.state.Gtime) {
-      this.setState({collapsedGirl: true})
+    if (prevState.Gtime !== this.state.Gtime) {
+      this.setState({ collapsedGirl: true })
     }
   }
 
-  onRegionChange(region) {
+  onRegionChangeB(region) {
     //console.log(region)
-    this.setState({collapsedGirl: false})
-    this.setState({Bcoord: region })
+    this.setState({ collapsedGirl: false })
+    this.setState({ Bcoord: region })
+  }
+  onRegionChangeG(region) {
+    //console.log(region)
+    this.setState({ collapsedGirl: false })
+    this.setState({ Gcoord: region })
   }
 
   render() {
     let staticData = GLOBAL.data;
- 
+
     const { multipleSelect, activeSections } = this.state;
     return (
       <View style={styles.container}>
@@ -93,43 +105,58 @@ export class marriage extends React.Component {
           {/*Content of Single Collapsible*/}
           <Collapsible collapsed={this.state.collapsedBoy} align="center">
             <View style={styles.content}>
-            <Grid>
-            <Row style={{ height: 60, backgroundColor: '#99e0d5' }}><Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}} style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><Text style={{left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టినరోజు</Text></Col>
-                  <Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><DatePicker
-                date={this.state.Bdate}
-                mode="date"
-                format="YYYY-MM-DD"
-                confirmBtnText="Confirm"
-                cancelBtnText="Cancel"
-                onDateChange={(date) => {this.setState({Bdate: date})}}/></Col></Row>
-            <Row style={{ height: 60, backgroundColor: '#99e0d5' }}><Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}} style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><Text style={{left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టిన సమయం</Text></Col>
-                  <Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><DatePicker
-                time={this.state.Btime}
-                mode="time"
-                confirmBtnText="Confirm"
-                cancelBtnText="Cancel"
-                onDateChange={(time) => {this.setState({Btime: time})}}/></Col></Row>
-             <Row style={{ height: 60, backgroundColor: '#99e0d5' }}><Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}} style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}>
-    <Text style={{left: 10, color: 'white', fontWeight: 'bold' }}>జన్మస్థానం</Text></Col><Col><Row><Text>{this.state.Bcoord.latitude}</Text></Row><Row><Text>{this.state.Bcoord.longitude}</Text></Row></Col>
-             </Row>
-            <Row style={{ height: 200, backgroundColor: '#99e0d5' }}>
-                  
+              <Grid>
+
+                {/* Start of Map*/}
+                <Row style={{ height: 60, backgroundColor: '#99e0d5' }}><Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }} style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}>
+                  <Text style={{ left: 10, color: 'white', fontWeight: 'bold' }}>జన్మస్థానం</Text></Col>
+                  {/* </Col><Col><Row><Text>{this.state.Bcoord.latitude}</Text></Row><Row><Text>{this.state.Bcoord.longitude}</Text></Row></Col> */}
+                </Row>
+                <Row style={{ height: 200, backgroundColor: '#99e0d5' }}>
+
                   <MapView
-         style={{ flex: 1 }}
-         
-         showsUserLocation
-         followUserLocation
-         onRegionChange={this.onRegionChange.bind(this)}
-         initialRegion={{
-         latitude: 17.68009,
-         longitude: 83.20161,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-         >
-           
-      </MapView>
-                  </Row>
-            </Grid>
+                    style={{ flex: 1 }}
+
+                    showsUserLocation
+                    followUserLocation
+                    onRegionChange={this.onRegionChangeB.bind(this)}
+                    initialRegion={{
+                      latitude: 17.68009,
+                      longitude: 83.20161,
+                      latitudeDelta: 0.0922,
+                      longitudeDelta: 0.0421
+                    }}
+                  >
+                    <MapView.Marker
+                      coordinate={{
+                        latitude: this.state.Bcoord.latitude,
+                        longitude: this.state.Bcoord.longitude
+                      }}
+                      title={"title"}
+                      description={"description"}
+                    />
+
+                  </MapView>
+                </Row>
+                {/* End of Map*/}
+
+                <Row style={{ height: 60, backgroundColor: '#99e0d5' }}><Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }} style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><Text style={{ left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టినరోజు</Text></Col>
+                  <Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><DatePicker
+                    date={this.state.Bdate}
+                    mode="date"
+                    format="YYYY-MM-DD"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    onDateChange={(date) => { this.setState({ Bdate: date }) }} /></Col></Row>
+                <Row style={{ height: 60, backgroundColor: '#99e0d5' }}><Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }} style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><Text style={{ left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టిన సమయం</Text></Col>
+                  <Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><DatePicker
+                    time={this.state.Btime}
+                    mode="time"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    onDateChange={(time) => { this.setState({ Btime: time }) }} /></Col></Row>
+
+              </Grid>
             </View>
           </Collapsible>
           {/*Code for Single Collapsible Ends*/}
@@ -143,27 +170,59 @@ export class marriage extends React.Component {
           {/*Content of Single Collapsible*/}
           <Collapsible collapsed={this.state.collapsedGirl} align="center">
             <View style={styles.content}>
-            <Grid>
-            <Row style={{ height: 60, backgroundColor: '#d095db' }}><Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}} style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><Text style={{left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టినరోజు</Text></Col>
-                  <Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><DatePicker
-                date={this.state.Gdate}
-                mode="date"
-                format="YYYY-MM-DD"
-                confirmBtnText="Confirm"
-                cancelBtnText="Cancel"
-                onDateChange={(date) => {this.setState({Gdate: date})}}/></Col></Row>
-            <Row style={{ height: 60, backgroundColor: '#d095db' }}><Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}} style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><Text style={{left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టిన సమయం</Text></Col>
-                  <Col style={{top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center'}}><DatePicker
-                time={this.state.Gtime}
-                mode="time"
-                confirmBtnText="Confirm"
-                cancelBtnText="Cancel"
-                onDateChange={(time) => {this.setState({Gtime: time})}}/></Col></Row>
-            </Grid>
+              <Grid>
+                {/* Start of Map*/}
+                <Row style={{ height: 60, backgroundColor: '#99e0d5' }}><Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }} style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}>
+                  <Text style={{ left: 10, color: 'white', fontWeight: 'bold' }}>జన్మస్థానం</Text></Col>
+                  {/* </Col><Col><Row><Text>{this.state.Bcoord.latitude}</Text></Row><Row><Text>{this.state.Bcoord.longitude}</Text></Row></Col> */}
+                </Row>
+                <Row style={{ height: 200, backgroundColor: '#99e0d5' }}>
+
+                  <MapView
+                    style={{ flex: 1 }}
+
+                    showsUserLocation
+                    followUserLocation
+                    onRegionChange={this.onRegionChangeG.bind(this)}
+                    initialRegion={{
+                      latitude: 17.68009,
+                      longitude: 83.20161,
+                      latitudeDelta: 0.0922,
+                      longitudeDelta: 0.0421
+                    }}
+                  >
+                    <MapView.Marker
+                      coordinate={{
+                        latitude: this.state.Gcoord.latitude,
+                        longitude: this.state.Gcoord.longitude
+                      }}
+                      title={"title"}
+                      description={"description"}
+                    />
+
+                  </MapView>
+                </Row>
+                {/* End of Map*/}
+                <Row style={{ height: 60, backgroundColor: '#d095db' }}><Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }} style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><Text style={{ left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టినరోజు</Text></Col>
+                  <Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><DatePicker
+                    date={this.state.Gdate}
+                    mode="date"
+                    format="YYYY-MM-DD"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    onDateChange={(date) => { this.setState({ Gdate: date }) }} /></Col></Row>
+                <Row style={{ height: 60, backgroundColor: '#d095db' }}><Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }} style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><Text style={{ left: 10, color: 'white', fontWeight: 'bold' }}>పుట్టిన సమయం</Text></Col>
+                  <Col style={{ top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center' }}><DatePicker
+                    time={this.state.Gtime}
+                    mode="time"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    onDateChange={(time) => { this.setState({ Gtime: time }) }} /></Col></Row>
+              </Grid>
             </View>
           </Collapsible>
           {/*Code for Single Collapsible Ends*/}
-          <Button title="Generate" type="clear"/>
+          <Button title="Generate" type="clear" />
         </ScrollView>
       </View>
     );
