f115c8d71c9b5486c711b7664133753d86bcce58
Ravi Shanker Reddy Karri <me@ravikarri.in>
2020-05-25 20:02:56 +0530

    Revert "Update data.js"
    This reverts commit 586ee1745e3c2f44ccf8bd192d2692fe3f7e3131.

--- screens/data.js
+++ screens/data.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "6d543638c0591dd96a98c7e1e62b316a510ae28c3c5fb8ba98a3cfdaa2d306b9",
+    token: "",
     url: "https://api.prokerala.com/v1/astrology/kundli-matching",
  };
