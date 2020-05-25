586ee1745e3c2f44ccf8bd192d2692fe3f7e3131
Ravi Shanker Reddy Karri <me@ravikarri.in>
2020-05-25 20:02:41 +0530

    Update data.js

--- screens/data.js
+++ screens/data.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "",
+    token: "6d543638c0591dd96a98c7e1e62b316a510ae28c3c5fb8ba98a3cfdaa2d306b9",
     url: "https://api.prokerala.com/v1/astrology/kundli-matching",
  };
